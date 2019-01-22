995e7d5956d7ca78b52a9306f533b6db4984987a
Maicon Hieronymus <mhierony@students.uni-mainz.de>
2019-01-22 17:49:38 +0100

    Added write_list and worked on 2D Jacobi. Unfortunately, the index is still off when sending (and receiving) data.

--- solution/aux/queue.h
+++ solution/aux/queue.h
@@ -59,6 +59,31 @@ write_notify_and_cycle
 	);
 
 void 
+write_list_notify_and_wait
+    ( const gaspi_number_t num
+        , gaspi_segment_id_t * const segment_id_local
+        , gaspi_offset_t * const offset_local
+        , gaspi_rank_t const rank
+        , gaspi_segment_id_t * const segment_id_remote
+        , gaspi_offset_t * const offset_remote
+        , gaspi_size_t * const size
+        , gaspi_notification_id_t const notification_id
+        , gaspi_notification_t const notification_value
+        , gaspi_queue_id_t const queue
+    );
+
+void write_list_and_wait
+    ( const gaspi_number_t num
+        , gaspi_segment_id_t * const segment_id_local
+        , gaspi_offset_t * const offset_local
+        , gaspi_rank_t const rank
+        , gaspi_segment_id_t * const segment_id_remote
+        , gaspi_offset_t * const offset_remote
+        , gaspi_size_t * const size
+        , gaspi_queue_id_t const queue
+    );
+    
+void 
 wait_for_flush_queue
         ( gaspi_queue_id_t const queue );
 
---
+++ solution/basic/jacobi_2d.cxx
@@ -0,0 +1,363 @@
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <limits>
+#include <string>
+#include <algorithm>
+
+#include <assert.h>
+#include <mpi.h>
+
+#include "aux/queue.h"
+#include "aux/success_or_die.h"
+#include "aux/waitsome.h"
+#include "aux/success_or_die.h"
+
+#include <GASPI.h>
+
+// #include "bitmap_IO.hpp"
+
+// sponsored by stack overflow: http://stackoverflow.com/questions/440133
+std::string random_string(size_t length) {
+
+    auto randchar = []() -> char {
+        const char charset[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+        return charset[ rand() % (sizeof(charset) - 1)];
+    };
+
+    srand(time(0));
+    std::string str(length, 0);
+    std::generate_n(str.begin(), length, randchar);
+    return str;
+}
+
+int main (int argc, char * argv[]) {
+    std::cout << "Start\n";
+    // Invoke MPI::Init before gaspi_proc_init !!!
+    MPI::Init (argc, argv);
+    std::cout << "MPI initialised\n";
+    SUCCESS_OR_DIE( gaspi_proc_init(GASPI_BLOCK) );
+    std::cout << "GASPI initialised\n";
+    const uint64_t height    =   3*4*5*6+2;
+    const uint64_t width     = 2*3*4*5*6+2;
+    const uint64_t num_ranks = MPI::COMM_WORLD.Get_size();
+    const uint64_t     rank  = MPI::COMM_WORLD.Get_rank();
+    const uint64_t radix     = uint64_t(std::sqrt(num_ranks));
+    const uint64_t verti     = rank / radix;
+    const uint64_t horiz     = rank % radix;
+    const bool     is_root   = rank == 0;
+
+    assert(num_ranks == radix*radix);
+    assert((height-2) % radix == 0);
+    assert((width -2) % radix == 0);
+    if( is_root ) 
+        std::cout << "Asserts done\n";
+    //////////////////////////////////////////////////////////////////////////
+    // GASPI SEGMENT 
+    //////////////////////////////////////////////////////////////////////////
+    gaspi_segment_id_t const segment_id = 0;
+    gaspi_segment_id_t const segment_id_root = 1;
+
+    const uint64_t local_height = (height-2)/radix+2;
+    const uint64_t local_width  = (width -2)/radix+2;
+    
+    // Create gaspi segments here for ying and yang
+    SUCCESS_OR_DIE( gaspi_segment_create(
+        segment_id,
+        local_height*local_width*2*sizeof(double), 
+        GASPI_GROUP_ALL, GASPI_BLOCK, GASPI_MEM_UNINITIALIZED
+      )
+    );
+    // Create pointer to segment
+    gaspi_pointer_t array;
+    SUCCESS_OR_DIE( gaspi_segment_ptr (segment_id, &array) );
+    // Use ying and yang again
+    double * ying = (double *) (array);
+    double * yang = ying + local_height*local_width;
+    
+    // make indexing great again!
+    auto       at = [&] (const uint64_t& row, const uint64_t& col) {
+        return row*width+col;
+    };
+    auto local_at = [&] (const uint64_t& row, const uint64_t& col) {
+        return row*local_width+col;
+    };
+    
+    if(is_root)
+    {
+        SUCCESS_OR_DIE( gaspi_segment_alloc(
+            segment_id_root, 
+            height*width*sizeof(double),
+            GASPI_MEM_UNINITIALIZED)
+        );
+        SUCCESS_OR_DIE( gaspi_segment_register(
+            segment_id_root,
+            rank,
+            GASPI_BLOCK)
+        );
+        
+//         SUCCESS_OR_DIE( gaspi_segment_create(
+//             segment_id_root,
+//             height*width*sizeof(double), 
+//             GASPI_GROUP_ALL, GASPI_BLOCK, GASPI_MEM_UNINITIALIZED
+//         )
+//         );
+        
+//         std::vector<double> image(height*width*is_root);
+        gaspi_pointer_t image_gaspi_ptr;
+        SUCCESS_OR_DIE( gaspi_segment_ptr (segment_id_root, &image_gaspi_ptr) );
+        double * image = (double *) (image_gaspi_ptr);
+        
+        // draw a checkerboard
+        const uint64_t stride = 181;
+        for (uint64_t row = 0; row < height; ++row)
+            for (uint64_t col = 0; col < width; ++col)
+                image[at(row, col)] = (row/stride + col/stride) % 2;
+        
+    
+    }
+    if( is_root ) 
+        std::cout << "Segments created\n";
+    //////////////////////////////////////////////////////////////////////////
+    // SCATTER
+    //////////////////////////////////////////////////////////////////////////
+
+    // We always write to the same segment
+    std::vector<gaspi_segment_id_t> segment_id_vec(local_height-2, segment_id);
+    // We read from segment 1 all the time
+    std::vector<gaspi_segment_id_t> segment_id_image_vec(local_height-2, segment_id_root);
+    std::vector<gaspi_offset_t> offset_local(local_height-2);
+    std::vector<gaspi_offset_t> offset_remote(local_height-2);
+    std::vector<gaspi_size_t> size(local_height, (local_width-2)*sizeof(double));
+    
+    gaspi_notification_id_t data_available = 0;
+    gaspi_queue_id_t queue_id = 0;
+    // Only root distributes data
+    if(is_root)
+    {
+        gaspi_offset_t offset_basis_remote = (local_width+1)*sizeof(double);
+        for(auto &off: offset_remote)
+        {
+            off = offset_basis_remote;
+            offset_basis_remote += local_width*sizeof(double);
+        }
+        for(gaspi_rank_t next_rank=1; next_rank<num_ranks; ++next_rank)
+        {
+            // The local offset for the current tile
+            gaspi_offset_t offset_basis_local = 
+                ( next_rank % radix ) * (local_width-2) 
+                + ( next_rank / radix ) * (local_height-2)*width;
+            // Always think in bytes!
+            offset_basis_local *= sizeof(double);
+
+            // Add the offset for every local_row to send
+            for(auto row=0; row<local_height-2; row++)
+            {
+                offset_local[row] = offset_basis_local;
+                offset_basis_local += width*sizeof(double);                
+            }
+            std::cout << "Sending to " << next_rank << "\n" << std::flush;
+            // Send the data
+            write_list_notify_and_wait(
+                local_height-2, segment_id_image_vec.data(), offset_local.data(),
+                next_rank, segment_id_vec.data(), offset_remote.data(), 
+                size.data(), data_available, next_rank+1, queue_id);
+        }
+        // We also need the data in the segment of root 
+        gaspi_offset_t offset_basis_local = 0;
+        for(auto row=0; row<local_height-2; row++)
+        {
+            offset_local[row] = offset_basis_local;
+            offset_basis_local += width*sizeof(double);                
+        }
+     
+//         std::cout << "rank " << rank << "sends from ";
+//         for(auto &off: offset_local)
+//             std::cout << off/sizeof(double) << ", ";
+//         std::cout << " to ";
+//         for(auto &off: offset_remote)
+//             std::cout << off/sizeof(double) << ", ";
+//         std::cout << std::endl << std::flush;
+    
+        
+        write_list_notify_and_wait(
+            local_height-2, segment_id_vec.data(), offset_local.data(), 
+            0, segment_id_vec.data(), offset_remote.data(), 
+            size.data(), data_available, 1, queue_id);
+    }
+    
+    // Every rank waits for its data and starts working 
+    wait_or_die(segment_id, data_available, rank+1);
+    if( is_root ) 
+        std::cout << "Data scattered\n";
+    //////////////////////////////////////////////////////////////////////////
+    // FIXPOINT COMPUTATION
+    //////////////////////////////////////////////////////////////////////////
+
+    auto update = [&] (const uint64_t& row, const uint64_t& col) {
+        return 0.25*(ying[local_at(row+1, col)] + ying[local_at(row-1, col)]
+                   + ying[local_at(row, col+1)] + ying[local_at(row, col-1)]);
+    };
+
+	// Halo above and below: local_height-2
+    // Data per row: 1
+    // Stride: local_width (duh!)
+    MPI::Datatype col_t = 
+    	MPI::DOUBLE.Create_vector (local_height-2, 1, local_width)
+                   .Create_resized(0, sizeof(double));
+    col_t.Commit();
+
+    uint64_t counter = 0, print_every = 1024;
+    double error = std::numeric_limits<double>::infinity();
+
+	uint64_t halo = local_width-2;
+    // Return at everywhere in the tile, even the halos
+    auto at_tile = [&] (const uint64_t row, const uint64_t& col) {
+    	// Halo offset for every row>0
+        // Column offset for the current row
+        // Offset for row after halo
+        assert(row != 0 || col < halo); // Out of bounds above
+        assert(row != local_height-1 || col < halo); // Out of bounds below
+        return (row==0) + col + row*local_width + (row==local_height-1);
+    };
+    while (error > 1E-4 && counter < 1UL << 14) {
+
+        MPI::Request req[8];
+		// There is a neighbour below so send it some juicy data
+        if (verti+1 < radix) {
+        	// Do *not* send the halo in the row, but only the data
+            req[0] = MPI::COMM_WORLD.Isend(&ying[at_tile(local_height-2, 1)],
+            	local_width-2, MPI::DOUBLE, rank+radix, 0);
+            // Receive at halo
+            req[1] = MPI::COMM_WORLD.Irecv(&ying[at_tile(local_height-1, 0)],
+            	local_width-2, MPI::DOUBLE, rank+radix, 0);
+        }
+		// Oh, there is a tile above us. Let's give him/her/it some data
+        if (verti > 0) {
+        	// Send data
+            req[2] = MPI::COMM_WORLD.Isend(&ying[at_tile(1, 1)], 
+            	local_width-2, MPI::DOUBLE, rank-radix, 0);
+            // Receive in halo
+            req[3] = MPI::COMM_WORLD.Irecv(&ying[at_tile(0,0)], 
+            	local_width-2, MPI::DOUBLE, rank-radix, 0);
+        }
+		// Oh my!!! There is a neighbour right of us.
+        if (horiz+1 < radix) {
+        	// Send data right of us
+            req[4] = MPI::COMM_WORLD.Isend(&ying[at_tile(1, local_width-2)],
+            	1, col_t, rank+1, 0);
+            req[5] = MPI::COMM_WORLD.Irecv(&ying[at_tile(1, local_width-1)],
+            	1, col_t, rank+1, 0);
+        }
+		// You know...
+        if (horiz > 0) {
+            req[6] = MPI::COMM_WORLD.Isend(&ying[at_tile(1, 1)], 
+            	1, col_t, rank-1, 0);
+            req[7] = MPI::COMM_WORLD.Irecv(&ying[at_tile(1, 0)],
+            	1, col_t, rank-1, 0); 
+        }
+
+        // relax every pixel in the interior
+        for (uint64_t row = 2; row < local_height-2; ++row)
+            for (uint64_t col = 2; col < local_width-2; ++col)
+                yang[local_at(row, col)] = update(row, col);
+
+        // Wait until data received
+        req[1].Wait(); req[3].Wait(); req[5].Wait(); req[7].Wait();
+
+        // fix the borders
+        for (uint64_t col = 2; col < local_width-2; ++col)
+            yang[local_at(1, col)] = update(1, col);
+        for (uint64_t col = 2; col < local_width-2; ++col)
+            yang[local_at(local_height-2, col)] = update(local_height-2, col);
+        for (uint64_t row = 1; row < local_height-1; ++row)
+            yang[local_at(row, 1)] = update(row, 1);
+        for (uint64_t row = 1; row < local_height-1; ++row)
+            yang[local_at(row, local_width-2)] = update(row, local_width-2);
+
+        // Wait until data send
+        req[0].Wait(); req[2].Wait(); req[4].Wait(); req[6].Wait();
+
+        double local_error = 0;
+        for (uint64_t row = 1; row < local_height-1; ++row) {
+            for (uint64_t col = 1; col < local_width-1; ++col) {
+                    const double residue = ying[local_at(row,col)]
+                                         - yang[local_at(row, col)];
+                    local_error += residue*residue;
+                    ying[local_at(row,col)] = yang[local_at(row, col)];
+            }
+        }
+
+        // every process needs the same error
+        MPI::COMM_WORLD.Allreduce(&local_error, &error, 1,
+                                  MPI::DOUBLE, MPI::SUM);
+
+        // status updates every print_every iteration
+        if (counter++ % print_every == print_every-1 && is_root)
+            std::cout << "# Squared error after " << counter
+                      << " iterations: " << error << std::endl;
+    }
+
+    // final status
+    if (is_root)
+        std::cout << "# Final squared error after " << counter
+                  << " iterations: " << error << std::endl;
+
+    //////////////////////////////////////////////////////////////////////////
+    // GATHER
+    //////////////////////////////////////////////////////////////////////////
+    // Local offset = iterate over local array
+    gaspi_offset_t offset_basis_local = (local_width+1)*sizeof(double);
+    for(auto row=0; row<local_height; row++)
+    {
+        offset_local[row] = offset_basis_local;
+        offset_basis_local += local_width*sizeof(double);                
+    }
+    // Remote offset = tile based indexing
+    gaspi_offset_t offset_basis_remote = 
+        ( rank % radix ) * (local_width-2) 
+        + ( rank / radix ) * (local_height-2)*width;
+    offset_basis_remote *= sizeof(double);
+    for(auto row=0; row<local_height-2; row++)
+    {
+        offset_remote[row] = offset_basis_remote;
+        offset_basis_remote += width*sizeof(double);                
+    }
+//     if(rank == 1)
+//     {
+//         std::cout << "rank " << rank << "sends from ";
+//         for(auto &off: offset_local)
+//             std::cout << off/sizeof(double) << ", ";
+//         std::cout << " to ";
+//         for(auto &off: offset_remote)
+//             std::cout << off/sizeof(double) << ", ";
+//         std::cout << std::endl << std::flush;
+//     }
+    // Send the data 
+    write_list_and_wait(
+        local_height-2, segment_id_vec.data(), offset_local.data(), 0, 
+        segment_id_image_vec.data(), offset_remote.data(), size.data(), 
+        queue_id);
+    std::cout << "Data is gathered\n";
+    SUCCESS_OR_DIE ( gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK) );
+    //////////////////////////////////////////////////////////////////////////
+    // CHECK
+    //////////////////////////////////////////////////////////////////////////
+
+    if (is_root) {
+        std::string  filename = random_string(8)+".bmp";
+//         dump_bitmap(image.data(), height, width, "www/"+filename);
+        std::cout << "# See http://iaimz105.informatik.uni-mainz.de/"
+                  << filename << "\nParallel programming is "
+                  << (counter == 8353 ? "fun!" : "error-prone!") << std::endl;
+    }
+    
+    SUCCESS_OR_DIE( gaspi_wait ( queue_id, GASPI_BLOCK ) );
+    SUCCESS_OR_DIE( gaspi_proc_term(GASPI_BLOCK) );
+  
+    MPI::Finalize();
+
+}
